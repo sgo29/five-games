ea4de5218fff7891e8b0d4095bdc0f1419d8fe96
sgo29 <sgq022992@gmail.com>
2020-06-16 12:21:28 -0400

    first commit

---
+++ Pong.py
@@ -0,0 +1,143 @@
+#'''Game set up'''
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by Sasha Go")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+'''Score'''
+score_a = 0
+score_b = 0
+
+'''Paddle A'''
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("green")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+'''Paddle B'''
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("purple")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+'''Ball'''
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 0.5
+ball.dy = 0.5
+
+'''Pen'''
+
+pen = turtle.Turtle()
+pen.speed(0)
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+
+
+
+
+''' Function'''
+'''left side'''
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+
+'''Right side'''
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+''' Keyboard Binding'''
+
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
+pen.color("green")
+pen.write("Player A: {} ".format(score_a), align="right", font=("Times New Roman", 12, "bold"))
+pen.color("purple")
+pen.write("Player B:{} ".format(score_b), align="left", font=("Times New Roman", 12, "bold"))
+
+'''Main game loop'''
+
+while True:
+    wn.update()
+    '''moves ball'''
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    scored = False
+    '''Border Checking'''
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_a += 1
+        scored = True
+
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_b += 1
+        scored = True
+
+
+    if scored:
+        pen.clear()
+        pen.color("green")
+        pen.write("Player A: {} ".format(score_a), align="right", font=("Times New Roman", 12, "bold"))
+        pen.color("purple")
+        pen.write("Player B:{} ".format(score_b), align="left", font=("Times New Roman", 12, "bold"))
+
+
+
+
+
+
+    '''ball collisions'''
+
+    if (ball.xcor() > 340 and ball.xcor() < 350)  and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
+        ball.setx(340)
+        ball.dx *= -1
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *= -1
